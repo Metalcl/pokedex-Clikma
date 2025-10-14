bd02477fe548a1f362837e3f50c0881c8d47c10f
metal.cl <ojedacasanueva@gmail.com>
2025-10-14 01:11:39 -0300

    Add Pages folder,Add pokedexPage, Add pokedex as a main path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PokedexPage } from './pages/pokedexPage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PokedexPage,
+  }
+];
